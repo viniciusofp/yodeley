e44e9fa462af7bc83b93c796772adc3c5a701e56
viniciusofp <viniciusofp@gmail.com>
2017-07-10 17:37:51 -0300

    remove padding top from cat page

--- themes/yodeley/header.php
+++ themes/yodeley/header.php
@@ -2,7 +2,7 @@
 	<head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title></title>
+	<title>Yodeley</title>
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 
 <!-- CSS -->
